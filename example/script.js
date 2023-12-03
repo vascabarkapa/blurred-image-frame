711e0d371dc14d66ed41004a1838cb32bf212d44
vascabarkapa <vascabarkapa@gmail.com>
2023-12-04 00:04:36 +0100

    fixed app script

--- example/script.js
+++ example/script.js
@@ -1,12 +1,15 @@
-function handleImage(e) {
-    const canvas = document.getElementById('outputCanvas');
+var originalExtension;
+var fileNameWithoutExtension;
+
+function handleImage(e, canvasId) {
+    const canvas = document.getElementById(canvasId);
     const ctx = canvas.getContext('2d');
 
     const image = new Image();
     image.src = URL.createObjectURL(e.target.files[0]);
 
     image.onload = function () {
-        const aspectRatio = 16 / 9;
+        const aspectRatio = canvas.width / canvas.height;
         const imageAspectRatio = image.width / image.height;
 
         let newWidth, newHeight;
@@ -26,20 +29,23 @@ function handleImage(e) {
 
         ctx.filter = 'blur(50px)';
         ctx.drawImage(image, -100, -100, canvas.width + 200, canvas.height + 200);
-        ctx.filter = 'none';
 
+        ctx.filter = 'none';
         ctx.drawImage(image, xOffset, yOffset, newWidth, newHeight);
+
+        originalExtension = e.target.files[0].name.split('.').pop();
+        fileNameWithoutExtension = e.target.files[0].name.replace(/\.[^/.]+$/, '');
     };
 }
 
-function downloadImage() {
-    const canvas = document.getElementById('outputCanvas');
+function downloadImage(canvasId) {
+    const canvas = document.getElementById(canvasId);
     const downloadLink = document.createElement('a');
 
-    downloadLink.href = canvas.toDataURL('image/png');
-    downloadLink.download = 'resized_image.png';
+    downloadLink.href = canvas.toDataURL(`image/${originalExtension}`);
+    downloadLink.download = `${fileNameWithoutExtension}_blurred.${originalExtension}`;
+
     document.body.appendChild(downloadLink);
     downloadLink.click();
-
     document.body.removeChild(downloadLink);
 }
